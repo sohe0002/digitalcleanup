2bd2ea576b9d15bc6e9a249e3daef0e1797392c3
Signe <sila0001@stud.kea.dk>
2024-09-17 11:39:40 +0200

    index

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,30 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-            colors: {
-                blå800: '#1C586E',     
-                blåtekst: '#062A37',   
-                rødorange: '#FB8984',
-                mørkerød: '#A82D28',
-                mørkerødtekst: '#550805',
-                lyseblå: '#DCF3F9',
-            },
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        blå700: "#18708B",
+        blå800: "#1C586E",
+        mørkeblåtekst: "#062A37",
+        rødorange: "#FB8984",
+        mørkerød: "#A82D28",
+        mørkerødtekst: "#550805",
+        lyseblå: "#DCF3F9",
+      },
 
-            fontFamily: {
-                poppins: ['Poppins', 'sans-serif'], // Definerer Poppins fonten
-              },
-              fontWeight: {
-                regular: '400', // Til Poppins Regular
-                bold: '700',    // Til Poppins Bold
-              }
-        },
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"], // Definerer Poppins fonten
+      },
+      fontWeight: {
+        regular: "400", // Til Poppins Regular
+        bold: "700", // Til Poppins Bold
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
